94f1c5c3014edf549fd90916a8889e4376bb4e4e
Mohammed Ayaanuddin <ayaanuddin.md@gmail.com>
2026-06-25 12:37:19 +0530

    Added first problem

---
+++ Arrays/Easy/check_if_array_sorted(1792).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1)return true;
+
+        int count=0;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]<nums[i-1])
+            {
+                ++count;
+                if(count>1)return false;
+            }
+        }   
+        if(nums[0]<nums[n-1])
+        {
+            ++count;
+        }
+        return count<=1;
+    }
+};
